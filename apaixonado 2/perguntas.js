b1dcd292de3be06ae0eb79c7b8a7628c5460c7f3
Fernando-modesto <fernando.souza.filho@escola.pr.gov.br>
2025-08-07 11:45:15 -0300

    Add files via upload

---
+++ apaixonado 2/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'futebol',
+    'recorde de mais bolas de ouro ?',
+    'Messi'
+)
+
+criaCartao(
+    'jiu-jitsu',
+    ' quem criou o jiu-jitsu brasileiro?',
+    'familia Gracie'
+)
+
+criaCartao(
+    'ufc',
+    'maior lutador do ufc?',
+    'jon jones'
+)
+
+criaCartao(
+    'escola',
+    'ano de criação do lindamir ?',
+    '2007'
+)
